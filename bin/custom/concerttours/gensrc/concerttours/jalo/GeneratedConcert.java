c273a1c5260c77b69c0a804a59eb473e87593464
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-11-12 18:44:47 -0200

    Extend the ServiceLayer

--- bin/custom/concerttours/gensrc/concerttours/jalo/GeneratedConcert.java
+++ bin/custom/concerttours/gensrc/concerttours/jalo/GeneratedConcert.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Nov 12, 2018 6:07:57 PM                     ---
+ * --- Generated at Nov 12, 2018 6:44:20 PM                     ---
  * ----------------------------------------------------------------
  */
 package concerttours.jalo;
